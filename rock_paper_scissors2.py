77917a664e1d7de2856ac7a38864efb5bbbf2403
Reanna Gibson <reannagibson@gmail.com>
2025-02-16 22:56:58 +0000

    EDIT: added more comments to homework

--- rock_paper_scissors2.py
+++ rock_paper_scissors2.py
@@ -1,39 +1,39 @@
-#importing the Random Module in order to use the random.randint method.
+#importing the Random Module
 import random
-#setting up dictionary
 
-#defining a function inorder for the computer to select randomly between rock, paper and scissors
+#Defining a function in-order for the computer to select randomly between rock, paper and scissors
 def get_computer_choice():
     """
     Randomly selects from Rock, Paper or Scissors for the computer
     :return: str
     """
-    return  random.choice (['Rock', 'Paper', 'Scissors'])
-#defines the function as a variable
+    return  random.choice (['Rock', 'Paper', 'Scissors']) #using.choice method to select one element.
+
+#Using a variable to call the function we created.
 computer_choice = get_computer_choice()
 
-#defining dictionary
+#Defining a dictionary to convert input to full names.
 choices = {'R':'Rock', 'P':'Paper', 'S':'Scissors'}
 
 #while loop is ensuring that the user input is valid.
 while True:
-    user_input = input("Lets play Rock, Paper, Scissors!\nTo start enter R, P or S:").upper()
-    if user_input in choices:
-        user_choice = choices[user_input] #converting user choice to the full word.
-        break #exit loop if input is valid
+    user_input = input("Lets play Rock, Paper, Scissors!\nTo start enter R, P or S:").upper() #using upper method to avoid case sensitivity issues
+    if user_input in choices: #Checks if input is valid
+        user_choice = choices[user_input] #Converting user choice to the full word.
+        break #Exit the loop when valid input is received
     else:
-        input("You have entered an invalid option. Let's try again!\nPlease input  R, P or S:")
+        input("You have entered an invalid option. Let's try again!\nPlease input  R, P or S:") #If input is invalid, prompts user to enter a valid inpout. The loop will continue until a correct input is given.
 
-#this is displaying what the user and computer have chosen
+#Displaying what the user and computer have chosen
 print(f"You have chosen: {user_choice}")
 print(f"The computer chose: {computer_choice}")
 
-#Determining the winner
-if user_choice == computer_choice: #if both choices are the same it's a draw
-    print("Draw")
+#Determining the winner based on rules
+if user_choice == computer_choice:
+    print("It's a Draw!") # Both choices are the same it's a draw
 elif (user_choice == "Rock" and computer_choice == "Scissors") or \
     (user_choice == "Scissors" and computer_choice == "Paper") or \
     (user_choice == "Paper" and computer_choice == "Rock"):
-    print("You won") #if the answers fit within this block of code the user won
+    print("You won! Congratulations! ") # Winning conditions
 else:
-    print("You lost!! Let's try again'") #otherwise computer wins!
+    print("You lost!! Let's play again ") #Losing conditions.
