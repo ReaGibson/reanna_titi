b066612a8075b3723f7fad61f313b6bb2ea6092f
Reanna Gibson <reannagibson@gmail.com>
2025-02-18 19:36:52 +0000

    EDIT: editted comments

--- rock_paper_scissors2.py
+++ rock_paper_scissors2.py
@@ -7,7 +7,7 @@ def get_computer_choice():
     Randomly selects from Rock, Paper or Scissors for the computer
     :return: str
     """
-    return  random.choice (['Rock', 'Paper', 'Scissors']) #using.choice method to select one element.
+    return  random.choice (['Rock', 'Paper', 'Scissors']) #using 'choice' method to select one element from the list.
 
 #Using a variable to call the function we created.
 computer_choice = get_computer_choice()
@@ -15,20 +15,20 @@ computer_choice = get_computer_choice()
 #Defining a dictionary to convert input to full names.
 choices = {'R':'Rock', 'P':'Paper', 'S':'Scissors'}
 
-#while loop is ensuring that the user input is valid.
+#This 'while' loop is ensuring that the user input is valid.
 while True:
-    user_input = input("Lets play Rock, Paper, Scissors!\nTo start enter R, P or S:").upper() #using upper method to avoid case sensitivity issues
+    user_input = input("Lets play Rock, Paper, Scissors!\nTo start enter R, P or S:").upper() #using 'upper' method to avoid case sensitivity issues
     if user_input in choices: #Checks if input is valid
         user_choice = choices[user_input] #Converting user choice to the full word.
         break #Exit the loop when valid input is received
     else:
         input("You have entered an invalid option. Let's try again!\nPlease input  R, P or S:") #If input is invalid, prompts user to enter a valid inpout. The loop will continue until a correct input is given.
 
-#Displaying what the user and computer have chosen
+#Displaying what the user and computer have chosen using 'f' string
 print(f"You have chosen: {user_choice}")
 print(f"The computer chose: {computer_choice}")
 
-#Determining the winner based on rules
+#Determining the winner based on rules of rock, paper, scissors
 if user_choice == computer_choice:
     print("It's a Draw!") # Both choices are the same it's a draw
 elif (user_choice == "Rock" and computer_choice == "Scissors") or \
