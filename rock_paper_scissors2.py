77755a04035d881b881bda6bff870dc8ada6c598
Reanna Gibson <reannagibson@gmail.com>
2025-02-18 20:11:00 +0000

    EDIT: adding more functions and fixed the get_user_choice function.

--- rock_paper_scissors2.py
+++ rock_paper_scissors2.py
@@ -1,39 +1,49 @@
-#importing the Random Module
+# Importing the random module to enable random selection for the computer's choice
 import random
 
-#Defining a function in-order for the computer to select randomly between rock, paper and scissors
+
+# Defining a function to randomly select between Rock, Paper, or Scissors for the computer
 def get_computer_choice():
     """
     Randomly selects from Rock, Paper or Scissors for the computer
     :return: str
     """
-    return  random.choice (['Rock', 'Paper', 'Scissors']) #using 'choice' method to select one element from the list.
+    return  random.choice (['Rock', 'Paper', 'Scissors']) # Using 'choice' method from the 'random' module to randomly select one option from the list
+
+# Defining a function to find user choice.
+def get_user_choice():
+    # Dictionary to link user input (R, P, S) to full word choices (Rock, Paper, Scissors)
+    choices = {'R': 'Rock', 'P': 'Paper', 'S': 'Scissors'}
+    # This 'while' loop is ensuring that the user input is valid.
+    while True:
+        user_input = input("Lets play Rock, Paper, Scissors!\nTo start enter R, P or S:").upper()  # Using the .upper() method to Convert user input to uppercase to handle both lowercase and uppercase inputs
+        if user_input in choices:  # Checks if user input is valid
+            user_input = choices[user_input]  # Converting user choice to the full word.
+            break  # Exit the loop when valid input is received
+        else:
+            print("You have entered an invalid option.")
+           # user_input = input("You have entered an invalid option. Let's try again!\nPlease input  R, P or S:")  # If input is invalid, prompts user to enter a valid input. The loop will continue until a correct input is given.
+    return user_input
+
+
+
+def determin_winner(user_choice, computer_choice):
+    # Determining the winner based on rules of rock, paper, scissors
+    # Displaying what the user and computer have chosen using 'f' string. Using the variable to call our function.
+    print(f"You have chosen: {user_choice}")
+    print(f"The computer chose: {computer_choice}")
+    if user_choice == computer_choice:
+        print("It's a Draw!")  # Both choices are the same it's a draw
+    elif (user_choice == "Rock" and computer_choice == "Scissors") or \
+            (user_choice == "Scissors" and computer_choice == "Paper") or \
+            (user_choice == "Paper" and computer_choice == "Rock"):
+        print("You won! Congratulations! ")  # Winning conditions
+    else:
+        print("You lost!! Let's play again ")  # Losing conditions.
 
 #Using a variable to call the function we created.
 computer_choice = get_computer_choice()
+user_choice = get_user_choice()
+determin_winner(user_choice, computer_choice)
 
-#Defining a dictionary to convert input to full names.
-choices = {'R':'Rock', 'P':'Paper', 'S':'Scissors'}
-
-#This 'while' loop is ensuring that the user input is valid.
-while True:
-    user_input = input("Lets play Rock, Paper, Scissors!\nTo start enter R, P or S:").upper() #using 'upper' method to avoid case sensitivity issues
-    if user_input in choices: #Checks if input is valid
-        user_choice = choices[user_input] #Converting user choice to the full word.
-        break #Exit the loop when valid input is received
-    else:
-        input("You have entered an invalid option. Let's try again!\nPlease input  R, P or S:") #If input is invalid, prompts user to enter a valid inpout. The loop will continue until a correct input is given.
-
-#Displaying what the user and computer have chosen using 'f' string
-print(f"You have chosen: {user_choice}")
-print(f"The computer chose: {computer_choice}")
-
-#Determining the winner based on rules of rock, paper, scissors
-if user_choice == computer_choice:
-    print("It's a Draw!") # Both choices are the same it's a draw
-elif (user_choice == "Rock" and computer_choice == "Scissors") or \
-    (user_choice == "Scissors" and computer_choice == "Paper") or \
-    (user_choice == "Paper" and computer_choice == "Rock"):
-    print("You won! Congratulations! ") # Winning conditions
-else:
-    print("You lost!! Let's play again ") #Losing conditions.
+#Could create a function for returning a string with winner
